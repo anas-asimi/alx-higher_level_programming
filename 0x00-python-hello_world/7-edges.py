ac684d85bc2b1fba6d4b4f6dbeacba9b51869d88
Anas Asimi <anas2020cr7@gmail.com>
2023-11-01 19:52:12 +0100

    aaaa

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-x = word[:3]
-y = word[-2:]
-z = word[1:-1]
-print("First 3 letters: {}".format(x))
-print("Last 2 letters: {}".format(y))
-print("Middle word: {}".format(z))
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
