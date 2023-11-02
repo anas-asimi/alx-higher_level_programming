bc4b03383b72e14627c1618136998e284de224b3
Anas Asimi <anas2020cr7@gmail.com>
2023-11-02 22:22:20 +0100

    a

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ from sys import argv
 
 if __name__ == "__main__":
     result = 0
-    for i in range(1, len(argv)):
-        result += int(argv[i])
-    print("{}".format(result))
+    for arg in argv[1:]:
+        result += int(arg)
+    print("{:d}".format(result))
