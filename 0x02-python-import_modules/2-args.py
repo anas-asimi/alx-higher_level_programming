ebbdc87a6e54e50d77730443b1e86b7d092a2c88
Anas Asimi <anas2020cr7@gmail.com>
2023-11-02 22:07:53 +0100

    a

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("0 arguments.")
     else:
-        print("{} arguments:".format(len(argv) - 1))
+        if len(argv) == 2:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
-            print("{}: {}".format(i,argv[i]))
+            print("{}: {}".format(i, argv[i]))
