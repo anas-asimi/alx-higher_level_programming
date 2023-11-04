f38ffd1b83724b4fcaf939649a403b9559fb93e4
Anas Asimi <anas2020cr7@gmail.com>
2023-11-04 19:42:44 +0100

    alx task

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     list_copy = my_list[:]
-    if idx >= 0 or idx < len(list_copy):
+    if idx >= 0 and idx < len(list_copy):
         list_copy[idx] = element
     return (list_copy)
