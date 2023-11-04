53e245263d6fe9f371ffd5b74c52431bf03aa393
Anas Asimi <anas2020cr7@gmail.com>
2023-11-04 13:53:52 +0100

    alx task

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    my_list = []
+    my_list.append(len(sentence))
+    my_list.append(sentence[0])
+    mu_tuple = my_list[:]
+    return (mu_tuple)
