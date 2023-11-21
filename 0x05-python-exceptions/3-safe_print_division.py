70ac6d1b7ac1ac87988cfb8a92bf795515ebbf7b
Anas Asimi <anas2020cr7@gmail.com>
2023-11-21 21:15:41 +0100

    alx task

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-	try:
-		x = a/b
-	except ZeroDivisionError:
-		x = None
-	finally:
-		print("Inside result: {}".format(x))
-	return(x)
+    try:
+        div = a / b
+    except ZeroDivisionError:
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return div
