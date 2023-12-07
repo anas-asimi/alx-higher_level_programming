1e552097c07e9826647f121e124c88779a1ec756
Anas Asimi <anas2020cr7@gmail.com>
2023-12-07 20:28:33 +0100

    alx task

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -11,4 +11,6 @@ class Student:
         self.age = age
 
     def to_json(self):
-        return {'first_name': self.first_name, 'last_name': self.last_name, 'age': self.age}
+        return {'first_name': self.first_name,
+                'last_name': self.last_name,
+                'age': self.age}
