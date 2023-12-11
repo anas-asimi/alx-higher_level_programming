426cd8d3a29addffec0e17dad53ece15aac9763f
Anas Asimi <anas2020cr7@gmail.com>
2023-12-11 14:55:00 +0100

    alx task

--- 0x0C-python-almost_a_circle/README.md
+++ 0x0C-python-almost_a_circle/README.md
@@ -1,4 +1,4 @@
 # 0x0C-python-almost_a_circle
 
-done :		13
-Not yet :	9
+done :		16
+Not yet :	6
--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,6 +23,18 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ to_json_string """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return ("[]")
         return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ save_to_file """
+        if list_objs is None:
+            pass
+        elif type(list_objs) is list and len(list_objs) > 0:
+            file_name = list_objs[0].__class__.__name__ + '.json'
+            list_dicts = [x.to_dictionary() for x in list_objs]
+            with open(file_name, 'w', encoding='utf-8') as f:
+                f.write(cls.to_json_string((list_dicts)))
