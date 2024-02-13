39e77ff5f34d874d5511294df7e2d626fdd707f2
Anas Asimi <anas2020cr7@gmail.com>
2024-02-13 11:22:09 +0100

    update

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
 const { argv } = process;
 
-if (argv[2] && !isNaN(Number(argv[2]))) {
-  console.log(`My number: ${Math.floor(Number(argv[2]))}`);
+if (argv[2]) {
+  if (isNaN(Number(argv[2]))) {
+    console.log('Not a number');
+  }
+  else {
+    console.log(`My number: ${Math.floor(Number(argv[2]))}`);
+  }
 } else {
   console.log('No argument');
 }
