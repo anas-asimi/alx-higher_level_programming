d8319c1913662253c774a56717dca2159f78da77
Anas Asimi <anas2020cr7@gmail.com>
2024-02-29 20:30:53 +0100

    fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-curl -i "$1" | head -n 5 | tail -1 | grep -o -E '[0-9]+'  
+curl -s "$1" | wc -c
